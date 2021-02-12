a473261c6ada75d11c0625f87fc4d4e3df13e58a
sgabriel190 <strilciuc_gabriel98@yahoo.ro>
2021-02-12 21:02:46 +0200

    Simple refactor.

---
+++ services/frontend/keep-fit/src/App/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component<any, any>{
+  render() {
+    return (
+        <div className={"container"}>
+            <h1>Hello world!</h1>
+        </div>
+    );
+  }
+}
+
+export default App;
