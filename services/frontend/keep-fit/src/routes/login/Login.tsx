32bfeb2e2601344c0806dbdb0120c70018f2b0fa
sgabriel190 <strilciuc_gabriel98@yahoo.ro>
2021-02-13 17:15:10 +0200

    Added minimal header and router.

---
+++ services/frontend/keep-fit/src/routes/login/Login.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './Login.css';
+
+class Login extends React.Component<any, any>{
+    render() {
+        return (
+            <div className={"container-login"}>
+                <h1>Login works!</h1>
+                <h1>Login works!</h1>
+                <h1>Login works!</h1>
+                <h1>Login works!</h1>
+                <h1>Login works!</h1>
+                v
+                <h1>Login works!</h1>
+                <h1>Login works!</h1>
+                <h1>Login works!</h1>
+                <h1>Login works!</h1>
+                <h1>Login works!</h1>
+                <h1>Login works!</h1>
+                <h1>Login works!</h1>
+                <h1>Login works!</h1>
+                v
+                <h1>Login works!</h1>
+                <h1>Login works!</h1>
+                <h1>Login works!</h1>
+                <h1>Login works!</h1>
+                v
+                <h1>Login works!</h1>
+                <h1>Login works!</h1>
+                <h1>Login works!</h1>
+                <h1>Login works!</h1>
+            </div>
+        );
+    }
+}
+
+export default Login;
---
+++ services/frontend/keep-fit/src/routes/register/Register.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Register.css';
+
+class Register extends React.Component<any, any>{
+    render() {
+        return (
+            <div className={"container-register"}>
+                <h1>Register works!</h1>
+            </div>
+        );
+    }
+}
+
+export default Register;
