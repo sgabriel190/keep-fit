d9974c9efa862cac457fb0ce9ed853408d11b63f
sgabriel190 <strilciuc_gabriel98@yahoo.ro>
2021-02-24 21:49:31 +0200

    Created web scraper.

---
+++ services/backend/recipe_web_scraper/extract-data-urls.py
@@ -0,0 +1,44 @@
+import json
+import multiprocessing
+import sys
+
+from recipe_scrapers import scrape_me
+
+
+def get_data(url: str, send_end):
+    print("Scrapping url:{}.".format(url))
+    data = scrape_me(url, wild_mode=True)
+    send_end.send(data.schema.data)
+
+
+def main():
+    file = open('data_source_urls/urls_input.txt', 'r')
+    output_file = open('./result/bulk_data.json', 'w')
+    url_list = list(map(lambda x: x[:-1], file.readlines()))
+    results = []
+    print("[INFO] - Urls were read.")
+    for url in range(0, len(url_list), 50):
+        jobs = []
+        pipe_list = []
+        for token in range(url, url+50):
+            if token >= len(url_list):
+                break
+            recv_end, send_end = multiprocessing.Pipe(False)
+            process = multiprocessing.Process(target=get_data, args=(url_list[token], send_end, ))
+            jobs.append(process)
+            pipe_list.append(recv_end)
+            process.start()
+        for process in jobs:
+            process.join()
+            print("[INFO] - {} finished.".format(process.name))
+        results += [x.recv() for x in pipe_list]
+    output_file.writelines(json.dumps(results, indent=4))
+
+
+if __name__ == "__main__":
+    import time
+    s = time.perf_counter()
+    main()
+    elapsed = (time.perf_counter() - s) / 60
+    print(f"[INFO] - script executed in {elapsed:0.2f} minutes.")
+    sys.exit(0)
