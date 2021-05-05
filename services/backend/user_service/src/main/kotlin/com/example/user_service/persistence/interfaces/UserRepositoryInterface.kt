3edf6d79749844fc3e9dacbf03b64199bc6309e9
sgabriel190 <strilciuc_gabriel98@yahoo.ro>
2021-05-05 19:59:15 +0300

    Updated documentation.

---
+++ services/backend/user_service/src/main/kotlin/com/example/user_service/persistence/interfaces/UserRepositoryInterface.kt
@@ -0,0 +1,4 @@
+package com.example.user_service.persistence.interfaces
+
+interface UserRepositoryInterface {
+}
---
+++ services/backend/user_service/src/main/kotlin/com/example/user_service/presentation/responses/HTTPResponse.kt
@@ -0,0 +1,3 @@
+package com.example.user_service.presentation.responses
+
+data class HTTPResponse()
