d35d7d7e7b88e4db8fa3c0ec247e073aadf14655
sgabriel190 <strilciuc_gabriel98@yahoo.ro>
2021-06-05 19:07:18 +0300

    Added populate script user.

---
+++ services/backend/database_scripts/sql_scripts/user/insert_data_user.sql
@@ -0,0 +1,12 @@
+INSERT INTO users(username, password, email, ID_user_details, ID_diet_plan)
+VALUES('gabriel', 'strilciuc', 'gabriel.strilciuc@gmail.com', 6, 6);
+INSERT INTO users(username, password, email, ID_user_details, ID_diet_plan)
+VALUES('alex', 'muraru', 'alex.muraru@gmail.com', 2, 2);
+INSERT INTO users(username, password, email, ID_user_details, ID_diet_plan)
+VALUES('alex', 'ilioi', 'alex.ilioi@gmail.com', 3, 3);
+INSERT INTO users(username, password, email, ID_user_details, ID_diet_plan)
+VALUES('vasile', 'enachi', 'vasile.enachi@gmail.com', 4, 4);
+INSERT INTO users(username, password, email, ID_user_details, ID_diet_plan)
+VALUES('bogdan', 'cojocariu', 'bogdan.cojocariu@gmail.com', 5, 5);
+
+SELECT * FROM users;
