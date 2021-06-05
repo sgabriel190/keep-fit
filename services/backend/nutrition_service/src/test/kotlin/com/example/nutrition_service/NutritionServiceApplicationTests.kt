5afcf3cc33ab3932e01c50d0097d1ad67b709071
sgabriel190 <strilciuc_gabriel98@yahoo.ro>
2021-06-05 19:26:54 +0300

    Added nutrition service.

---
+++ services/backend/nutrition_service/src/test/kotlin/com/example/nutrition_service/NutritionServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.nutrition_service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NutritionServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
