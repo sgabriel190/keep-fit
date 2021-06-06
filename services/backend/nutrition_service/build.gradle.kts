84105fb109c38b5271f5afe77ac86500ffdde6a3
sgabriel190 <strilciuc_gabriel98@yahoo.ro>
2021-06-07 00:53:37 +0300

    Updated exposed version.

--- services/backend/nutrition_service/build.gradle.kts
+++ services/backend/nutrition_service/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.4.4"
+    id("org.springframework.boot") version "2.5.0"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.5.10"
     kotlin("plugin.spring") version "1.5.10"
@@ -10,6 +10,8 @@ plugins {
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
+val exposedVersion: String by project
+val sqliteVersion: String by project
 
 repositories {
     mavenCentral()
@@ -18,12 +20,12 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
 
-    implementation("org.jetbrains.exposed:exposed-core:0.30.2")
-    implementation("org.jetbrains.exposed:exposed-dao:0.30.2")
-    implementation("org.jetbrains.exposed:exposed-jdbc:0.30.2")
-    implementation("org.xerial:sqlite-jdbc:3.21.0")
-    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.xerial:sqlite-jdbc:$sqliteVersion")
+    implementation("org.springframework.boot:spring-boot-starter-web:2.5.0")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
