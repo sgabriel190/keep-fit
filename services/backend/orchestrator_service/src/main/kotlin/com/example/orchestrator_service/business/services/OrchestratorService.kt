15fe289365677835c5b554d954fea675e3eb3cdb
sgabriel190 <strilciuc_gabriel98@yahoo.ro>
2021-06-14 20:48:39 +0300

    Created orchestrator service.

---
+++ services/backend/orchestrator_service/src/main/kotlin/com/example/orchestrator_service/business/services/OrchestratorService.kt
@@ -0,0 +1,6 @@
+package com.example.orchestrator_service.business.services
+
+import com.example.orchestrator_service.business.interfaces.OrchestratorServiceInterface
+
+class OrchestratorService: OrchestratorServiceInterface {
+}
