e938850dd4d9ebaa7a6eb85b0ad05191e99c5153
sgabriel190 <strilciuc_gabriel98@yahoo.ro>
2021-06-19 23:33:28 +0300

    Updated plan service.

--- services/backend/plan_service/src/main/java/com/example/plan_service/presentation/controllers/PlanController.java
+++ services/backend/plan_service/src/main/java/com/example/plan_service/presentation/controllers/PlanController.java
@@ -5,15 +5,16 @@ import com.example.plan_service.persistence.pojo.MealModel;
 import com.example.plan_service.persistence.pojo.MealRecipeModel;
 import com.example.plan_service.persistence.pojo.MenuModel;
 import com.example.plan_service.persistence.pojo.PlanModel;
-import com.example.plan_service.persistence.repositories.MealRepository;
 import com.example.plan_service.presentation.http.MyError;
 import com.example.plan_service.presentation.http.Response;
 import com.example.plan_service.presentation.requests.UserPlanRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api/planning")
@@ -24,81 +25,95 @@ public class PlanController {
         this.planService = planService;
     }
 
+    @Async
     @RequestMapping(value = "/ping", method = RequestMethod.GET)
-    public ResponseEntity<Object> ping(){
+    @ResponseBody
+    public CompletableFuture<ResponseEntity<Object>> ping(){
         Response<Object> result = new Response<>(true, 200, null, "", "");
-        return new ResponseEntity<>(result, HttpStatus.valueOf(result.getCode()));
+        return CompletableFuture.completedFuture(new ResponseEntity<>(result, HttpStatus.valueOf(result.getCode())));
     }
 
+    @Async
     @RequestMapping(value = "/meal", method = RequestMethod.GET)
-    public ResponseEntity<Object> getMeals(){
+    @ResponseBody
+    public CompletableFuture<ResponseEntity<Object>> getMeals(){
         Response<List<MealModel>> result = planService.getMeals();
         if (result.getSuccessfulOperation()){
-            return new ResponseEntity<>(result, HttpStatus.valueOf(result.getCode()));
+            return CompletableFuture.completedFuture(new ResponseEntity<>(result, HttpStatus.valueOf(result.getCode())));
         }
         else{
             MyError tmp = new MyError(result.getCode(), result.getMessage());
-            return new ResponseEntity<>(tmp, HttpStatus.valueOf(tmp.getCode()));
+            return CompletableFuture.completedFuture(new ResponseEntity<>(tmp, HttpStatus.valueOf(tmp.getCode())));
         }
     }
 
+    @Async
     @RequestMapping(value = "/plan", method = RequestMethod.GET)
-    public ResponseEntity<Object> getPlans(){
+    @ResponseBody
+    public CompletableFuture<ResponseEntity<Object>> getPlans(){
         Response<List<PlanModel>> result = planService.getPlans();
         if (result.getSuccessfulOperation()){
-            return new ResponseEntity<>(result, HttpStatus.valueOf(result.getCode()));
+            return CompletableFuture.completedFuture(new ResponseEntity<>(result, HttpStatus.valueOf(result.getCode())));
         }
         else{
             MyError tmp = new MyError(result.getCode(), result.getMessage());
-            return new ResponseEntity<>(tmp, HttpStatus.valueOf(tmp.getCode()));
+            return CompletableFuture.completedFuture(new ResponseEntity<>(tmp, HttpStatus.valueOf(tmp.getCode())));
         }
     }
 
+    @Async
     @RequestMapping(value = "/plan/user/{idUser}", method = RequestMethod.POST)
-    public ResponseEntity<Object> addUserPlan(@PathVariable Integer idUser, @RequestBody UserPlanRequest data){
+    @ResponseBody
+    public CompletableFuture<ResponseEntity<Object>> addUserPlan(@PathVariable Integer idUser, @RequestBody UserPlanRequest data){
         Response<PlanModel> result = planService.createUserPlan(idUser, data);
         if (result.getSuccessfulOperation()){
-            return new ResponseEntity<>(result, HttpStatus.valueOf(result.getCode()));
+            return CompletableFuture.completedFuture(new ResponseEntity<>(result, HttpStatus.valueOf(result.getCode())));
         }
         else{
             MyError tmp = new MyError(result.getCode(), result.getMessage());
-            return new ResponseEntity<>(tmp, HttpStatus.valueOf(tmp.getCode()));
+            return CompletableFuture.completedFuture(new ResponseEntity<>(tmp, HttpStatus.valueOf(tmp.getCode())));
         }
     }
 
+    @Async
     @RequestMapping(value = "/plan/user/{idUser}", method = RequestMethod.GET)
-    public ResponseEntity<Object> getPlan(@PathVariable Integer idUser){
+    @ResponseBody
+    public CompletableFuture<ResponseEntity<Object>> getPlan(@PathVariable Integer idUser){
         Response<PlanModel> result = planService.getPlan(idUser);
         if (result.getSuccessfulOperation()){
-            return new ResponseEntity<>(result, HttpStatus.valueOf(result.getCode()));
+            return CompletableFuture.completedFuture(new ResponseEntity<>(result, HttpStatus.valueOf(result.getCode())));
         }
         else{
             MyError tmp = new MyError(result.getCode(), result.getMessage());
-            return new ResponseEntity<>(tmp, HttpStatus.valueOf(tmp.getCode()));
+            return CompletableFuture.completedFuture(new ResponseEntity<>(tmp, HttpStatus.valueOf(tmp.getCode())));
         }
     }
 
+    @Async
     @RequestMapping(value = "/menu", method = RequestMethod.GET)
-    public ResponseEntity<Object> getMenus(){
+    @ResponseBody
+    public CompletableFuture<ResponseEntity<Object>> getMenus(){
         Response<List<MenuModel>> result = planService.getMenus();
         if (result.getSuccessfulOperation()){
-            return new ResponseEntity<>(result, HttpStatus.valueOf(result.getCode()));
+            return CompletableFuture.completedFuture(new ResponseEntity<>(result, HttpStatus.valueOf(result.getCode())));
         }
         else{
             MyError tmp = new MyError(result.getCode(), result.getMessage());
-            return new ResponseEntity<>(tmp, HttpStatus.valueOf(tmp.getCode()));
+            return CompletableFuture.completedFuture(new ResponseEntity<>(tmp, HttpStatus.valueOf(tmp.getCode())));
         }
     }
 
+    @Async
     @RequestMapping(value = "/mealRecipe", method = RequestMethod.GET)
-    public ResponseEntity<Object> getMealRecipe(){
+    @ResponseBody
+    public CompletableFuture<ResponseEntity<Object>> getMealRecipe(){
         Response<List<MealRecipeModel>> result = planService.getMealRecipes();
         if (result.getSuccessfulOperation()){
-            return new ResponseEntity<>(result, HttpStatus.valueOf(result.getCode()));
+            return CompletableFuture.completedFuture(new ResponseEntity<>(result, HttpStatus.valueOf(result.getCode())));
         }
         else{
             MyError tmp = new MyError(result.getCode(), result.getMessage());
-            return new ResponseEntity<>(tmp, HttpStatus.valueOf(tmp.getCode()));
+            return CompletableFuture.completedFuture(new ResponseEntity<>(tmp, HttpStatus.valueOf(tmp.getCode())));
         }
     }
 }
