d7442dd877fd83917df376eb8508b94b0b359111
sgabriel190 <strilciuc_gabriel98@yahoo.ro>
2021-06-21 15:03:22 +0300

    Updated email service.

--- services/backend/email_service/SMTPServer.py
+++ services/backend/email_service/SMTPServer.py
@@ -9,14 +9,11 @@ class SMTPServer:
     def __init__(self, username: str, password: str):
         self.__context = ssl.create_default_context()
         self.__port: int = 465
+        self.__password = password
         self.__smtp_server: str = "smtp.gmail.com"
         self.__sender_email: str = username
-        self.__connection = smtplib.SMTP_SSL(
-            self.__smtp_server,
-            self.__port,
-            context=self.__context
-        )
-        self.__connection.login(self.__sender_email, password)
+        self.__connection = None
+        self.smtp_connect()
 
     def send_email(self, receiver_email: str, message: str, subject: str) -> None:
         tmp = MIMEMultipart("alternative")
@@ -27,7 +24,19 @@ class SMTPServer:
         msg_part_plain = MIMEText(message, "plain")
         tmp.attach(msg_part_plain)
         tmp.attach(msg_part_html)
-        self.__connection.sendmail(self.__sender_email, receiver_email, tmp.as_string())
+        try:
+            self.__connection.sendmail(self.__sender_email, receiver_email, tmp.as_string())
+        except Exception:
+            self.smtp_connect()
+            self.__connection.sendmail(self.__sender_email, receiver_email, tmp.as_string())
+
+    def smtp_connect(self) -> None:
+        self.__connection = smtplib.SMTP_SSL(
+            self.__smtp_server,
+            self.__port,
+            context=self.__context
+        )
+        self.__connection.login(self.__sender_email, self.__password)
 
     def close_connection(self) -> None:
         self.__connection.close()
