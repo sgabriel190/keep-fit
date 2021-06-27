59f54adbdd0810df0917f5c9732d8b8d0f28a8b0
sgabriel190 <strilciuc_gabriel98@yahoo.ro>
2021-06-28 02:13:22 +0300

    Updated nutrtition service

--- services/backend/nutrition_service/src/main/kotlin/com/example/nutrition_service/persistence/tables/Images.kt
+++ services/backend/nutrition_service/src/main/kotlin/com/example/nutrition_service/persistence/tables/Images.kt
@@ -5,6 +5,6 @@ import org.jetbrains.exposed.sql.Column
 
 object Images: IntIdTable() {
     val imagePath: Column<String>   = text("image_path")
-    val idRecipe                    = reference("ID_recipe", Recipes)
+    val recipe                      = reference("ID_recipe", Recipes)
     override val tableName: String  = "images"
 }
